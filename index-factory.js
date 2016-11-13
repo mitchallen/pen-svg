6876b653048f804206ad2b62ce7ac521b795387c
Mitch Allen <mitch.c.allen@gmail.com>
2016-11-13 11:15:11 -0700

    added getSVG method

--- index-factory.js
+++ index-factory.js
@@ -7,15 +7,114 @@
 
 "use strict";
 
+var fs = require('fs'),
+    util = require('util'),
+    demand = require('@mitchallen/demand'),
+    fuseFactory = require("@mitchallen/fuse-svg-path"),
+    fuse = fuseFactory.create({});
+
 module.exports.create = (spec) => {
     if(!spec) {
         return null;
     }
     // private 
     let _package = "@mitchallen/pen-svg";
+    var _pen = [];
     return {
         // public 
         package: () => _package,
-        health: () => "OK"
+        health: () => "OK",
+
+        addPen: p => _pen.push(p),
+
+        getSVG: function(options) {
+
+            if(!options) {
+                return null;
+            }
+
+            var svgWidth  = options.width   || "100";
+            var svgHeight = options.height  || "100";
+            var title     = options.title   || "pen-svg file";
+            var desc      = options.desc    || "code generated svg file";
+            var groupId   = options.groupId || "g1";
+            var xScale    = options.xScale  || 1;
+            var yScale    = options.yScale  || 1;
+            var xTranslate = options.xTranslate  || 0;
+            var yTranslate = options.yTranslate  || 0;
+            var maxValve = options.maxValve  || 1000;
+
+            // Generate file content
+
+            let fd = util.format('<svg xmlns="http://www.w3.org/2000/svg" width="%s" height="%s">\n', svgWidth, svgHeight);
+
+            fd += util.format('  <title>%s</title>\n', title );
+            fd += util.format('  <desc>%s</desc>\n', desc );
+
+            fd += util.format(
+                '  <g id="%s" transform="scale(%d,%d) translate(%d,%d)">\n', groupId, xScale, yScale, xTranslate, yTranslate );
+
+            function zfill(strNum, len) {return (Array(len).join("0") + strNum).slice(-len);}
+
+            _pen.forEach( item => {
+                // fd += util.format('    <path fill="none" stroke="#F00" stroke-width="0.25px" d="%s" />\n', path );
+
+                var sPath = "";
+
+                let penPath = fuse.removeDupes( { 
+                    path: item.path(),
+                    maxValve: maxValve 
+                });
+
+                for( var key in penPath ) {
+                    let point = penPath[key];
+                    sPath += util.format("%s%d %d%s", 
+                    point.op == "L" ? "" : point.op, 
+                    point.x, point.y, 
+                    key == penPath.length - 1 ? "" : " ");
+                }
+
+                // TODO: stroke-linecap, stroke-linejoin
+
+                var hexColor = zfill( item.color().toString(16), 6);
+
+                fd += util.format(
+                    '    <path fill="none" stroke="#%s" stroke-width="%d" d="%s" />\n', hexColor, item.width(), sPath );
+
+            });
+     
+            fd += '  </g>\n';
+            fd += '</svg>';
+
+            return fd;
+        },
+
+        writeSVG: options => {
+
+            demand.notNull(options,"writeSVG method requires parameters");
+            demand.notNull(options.filename,"writeSVG method requires spec.filename parameter");
+            demand.notNull(options.width,"writeSVG method requires spec.width parameter");
+
+            var filename = options.filename,
+                title = spec.title || "pen-svg file",
+                desc = spec.desc || "(c) 2016 Mitch Allen",
+                groupId = spec.groupId;
+
+            var getOps = {
+                title: title,
+                desc: desc,
+                groupId: groupId,
+            };
+
+            var stream = fs.createWriteStream(filename);
+            var fData = this.getSVG(getOps);
+            stream.write(fData);
+            // stream.close();  // would randomly gen bad descriptor message
+            stream.end();
+            console.log("data written to: ", filename);
+            return fData;
+
+        }
+
     };
 };
--- test/smoke-test-factory.js
+++ test/smoke-test-factory.js
@@ -8,6 +8,7 @@
 
 var request = require('supertest'),
     should = require('should'),
+    penFactory = require("@mitchallen/pen"),
     modulePath = "../index-factory";
 
 describe('module factory smoke test', () => {
@@ -57,6 +58,70 @@ describe('module factory smoke test', () => {
         var obj = _factory.create({});
         should.exist(obj);
         obj.health().should.eql("OK");
+        done();
+    });
+
+    it('getSVG should return svg for a pen', done => {
+        var obj = _factory.create({});
+        should.exist(obj);
+
+        var pen = penFactory.create({ 
+            color: 0xFF0000,    // red pen
+            width: 2,           // pen width 
+            alpha: 0.8          // pen alpha value
+        });
+
+        pen.up();
+        pen.goto( { x: 10, y: 15 } );   // MoveTo 50, 50
+        pen.down();
+        pen.goto( { x: 20, y: 25 } );   // LineTo 10, 20
+        pen.goto( { x: 30, y: 35 } );   // LineTo 15, 30
+        pen.goto( { x: 40, y: 45 } );   // LineTo 45, 60
+        pen.up();
+        pen.goto( { x: 50, y: 55 } );   // MoveTo 30, 40
+        pen.down();
+        pen.goto( { x: 60, y: 65 } );   // LineTo 10, 20
+
+        obj.addPen(pen);
+
+        var svg = obj.getSVG({});
+        console.log("SVG: \n", svg);
+        done();
+    });
+
+    it('getSVG should return svg for a multiple pens', done => {
+        var obj = _factory.create({});
+        should.exist(obj);
+
+        var pen1 = penFactory.create({ 
+            color: 0xFF0000,    // red pen
+            width: 2,           // pen width 
+            alpha: 0.8          // pen alpha value
+        });
+
+        var pen2 = penFactory.create({ 
+            color: 0x0000FF,    // blue pen
+            width: 4,           // pen width 
+            alpha: 0.8          // pen alpha value
+        });
+
+        pen1.up();
+        pen1.goto( { x: 10, y: 15 } );   // MoveTo 50, 50
+        pen1.down();
+        pen1.goto( { x: 20, y: 25 } );   // LineTo 10, 20
+        pen1.goto( { x: 30, y: 35 } );   // LineTo 15, 30
+        
+        pen2.up();
+        pen2.goto( { x: 40, y: 45 } );   // LineTo 45, 60
+        pen2.down();
+        pen2.goto( { x: 50, y: 55 } );   // MoveTo 30, 40
+        pen2.goto( { x: 60, y: 65 } );   // LineTo 10, 20
+
+        obj.addPen(pen1);
+        obj.addPen(pen2);
+
+        var svg = obj.getSVG({});
+        console.log("SVG: \n", svg);
         done();
     });
 });
