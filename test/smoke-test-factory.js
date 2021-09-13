fefe01f74a901040674d29da2af5162953ca722c
Mitch Allen <github@mitchallen.com>
2021-09-12 22:46:23 -0400

    updated deps

--- test/smoke-test-factory.js
+++ test/smoke-test-factory.js
@@ -166,88 +166,47 @@ describe('module factory smoke test', () => {
 
     // random
 
-    it('writeSVG should write random walk svg', done => {
+    it('writeSVG should write random svg', done => {
         let obj = _factory.create({});
         should.exist(obj);
 
-        let pen0 = penFactory.create({ 
-            color: 0xFFFFFF,    
-            fill: 0xFFFFFF,
-            width: 2,           // pen width 
-            alpha: 1.0,          // pen alpha value
-        });
-
-        let pen1 = penFactory.create({ 
+        let pen = penFactory.create({ 
             color: 0xFF0000,    // red pen
             width: 2,           // pen width 
             alpha: 0.8          // pen alpha value
         });
 
-        let pen2 = penFactory.create({ 
-            color: 0x0000FF,    // blue pen
-            width: 4,           // pen width 
-            alpha: 0.8          // pen alpha value
-        });
-
-        let size = 2048;
+        let xSize = 1024;
+        let ySize = 1024;
         let padding = 50;
-        let cursor1 = { x: size / 2 , y: size / 2 };
-        let cursor2 = cursor1;
+        let cursor = { x: xSize / 2 , y: ySize / 2 };
         let distance = 100;
-        let limit = 2000;
+        let limit = 200;
 
-        pen0.up();
-        pen0.goto( { x: 0, y: 0 });  
-        pen0.down();
-        pen0.goto( { x: size, y: 0 } );
-        pen0.goto( { x: size, y: size } );
-        pen0.goto( { x: 0, y: size } );
-
-        pen1.up();
-        pen1.goto( cursor1 );  
-        pen1.down();
-
-        pen2.up();
-        pen2.goto( cursor2 );  
-        pen2.down();
+        pen.up();
+        pen.goto( cursor );  
+        pen.down();
 
         for( let i = 0; i < limit; i++ ) {
-
-            let c1 = cursor1;
+            let c1 = cursor;
             c1.x += chance.integer( { min: -distance, max: distance });
             c1.y += chance.integer( { min: -distance, max: distance });
             if( 
                 c1.x > padding 
-                && c1.x < (size - padding) 
+                && c1.x < (xSize - padding) 
                 && c1.y > padding 
-                && c1.y < (size - padding)
+                && c1.y < (ySize - padding)
             ) {
-                cursor1 = c1;
-                pen1.goto( cursor1 );
+                cursor = c1;
+                pen.goto( cursor );
             }
-
-            let c2 = cursor2;
-            c2.x += chance.integer( { min: -distance, max: distance });
-            c2.y += chance.integer( { min: -distance, max: distance });
-            if( 
-                c2.x > padding 
-                && c2.x < (size - padding) 
-                && c2.y > padding 
-                && c2.y < (size - padding)
-            ) {
-                cursor2 = c2;
-                pen2.goto( cursor2 );
-            }
-
         }
 
-        obj.addPen(pen0);
-        obj.addPen(pen1);
-        obj.addPen(pen2);
+        obj.addPen(pen);
 
         let svg = obj.writeSVG({ 
-            width: 2048,
-            height: 2048,
+            width: 1024,
+            height: 1024,
             filename: "test/output/random-test.svg" 
         });
         // console.log("SVG: \n", svg);
